4925031140dd16aa89ee2878d962099c73660673
cox4 <lyhlcong@gmail.com>
2019-03-31 19:36:43 -0400

    board already

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import {ShotChart} from './ShotChart';
-
+import {Profile} from './Profile';
+import nba from 'nba';
 export class Main extends React.Component {
+  state = {
+    playerId: nba.findPlayer('Lebron James').playerId,
+    playerInfo: {},
+  }
+  componentDidMount() {
+    nba.stats.playerInfo({PlayerID: this.state.playerId})
+      .then((info) => {
+        console.log("player info log", info);
+        const playerInfo = Object.assign({}, info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+        console.log('playerInfo', playerInfo);
+        this.setState({playerInfo});
+    })
+      .catch((e) => console.log(e))
+
+  }
+
   render () {
     return(
-      <div>
-        <ShotChart playerId={2544}/>
+      <div className="main">
+        <Profile playerInfo={this.state.playerInfo}/>
+        <ShotChart playerId={this.state.playerId}/>
       </div>
     );
   }
